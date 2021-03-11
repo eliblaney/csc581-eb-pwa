5d75981479551a4ebd9218cf0208bab71be0e19a
Eli Blaney <eliblaney@gmail.com>
2021-03-11 13:20:42 -0600

    Add service worker

---
+++ sw.js
@@ -0,0 +1,31 @@
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+// Resources that are always cached
+const PRECACHE_URLS = [
+	'index.html',
+	'./',
+	'css/style.css',
+	'js/main.js',
+	'sw.js',
+];
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(PRECACHE).then(cache => {
+			return cache.addAll(PRECACHE_URLS);
+		})
+	);
+});
+
+self.addEventListener('activate', event => {
+	console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			return response || fetch(event.request);
+		});
+	);
+});
