326293d71bb095fc9d0b6f65ee454d6f907d249f
Eli Blaney <eliblaney@gmail.com>
2021-03-18 14:37:47 -0500

    Add more birds and alerts

--- js/main.js
+++ js/main.js
@@ -10,3 +10,51 @@ function registerServiceWorker() {
 		});
 	}
 }
+
+STATE_BIRDS = {
+	'California Gull': [
+		'California',
+		'Utah',
+	],
+	'Cardinal': [
+		'Illinois',
+		'Indiana',
+		'Kentucky',
+		'North Carolina',
+		'Ohio',
+		'Virginia',
+		'West Virginia'
+	],
+	'Chicken': [
+		'Delaware',
+		'Rhode Island'
+	],
+	'Meadowlark': [
+		'Kansas',
+		'Montana',
+		'Nebraska',
+		'North Dakota',
+		'Oregon',
+		'Wyoming'
+	],
+	'Mockingbird': [
+		'Florida',
+		'Mississippi',
+		'Tennessee',
+		'Texas'
+	]
+}
+
+function openState(name, filename) {
+	if(filename === undefined) {
+		filename = name;
+	}
+	swal({
+		title: name + ' States:',
+		text: STATE_BIRDS[name].join("\n"),
+		icon: "images/" + filename + ".png",
+		button: {
+			text: "Close"
+		}
+	});
+}
